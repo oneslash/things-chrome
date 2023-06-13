591c0135009e6128cdab70204116150f4e2e7e9b
Sardorbek Pulatov <sardo@hey.com>
2023-06-13 21:18:23 +0200

    Add context menu so people can add text or link into the Things 3 App

--- scripts/background.js
+++ scripts/background.js
@@ -2,6 +2,22 @@ chrome.action.onClicked.addListener(function (activeTab) {
     const title = activeTab.title;
     const url = activeTab.url;
 
-    var newURL = `things:///add?title=${title}&notes=Link: ${url}&show-quick-entry=true`;
+    var newURL = `things:///add?title=${title}&show-quick-entry=true&notes=Link:${url}`;
     chrome.tabs.create({ url: newURL });
 });
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.contextMenus.create({
+        title: "Add to Things",
+        contexts: ["link", "selection"],
+        id: "contextMenu",
+    });
+});
+
+chrome.contextMenus.onClicked.addListener(addLinkToThings);
+
+function addLinkToThings(info) {
+    var newURL = `things:///add?title=${info.selectionText}&show-quick-entry=true&notes=Link:${info.linkUrl}`;
+    chrome.tabs.create({ url: newURL });
+}
+
